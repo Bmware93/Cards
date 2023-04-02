45cc7d73152a026c004aab440a9a483c01a639c7
Benia Morgan <bmorganware@gmail.com>
2023-04-02 09:51:03 -0400

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Benia Morgan-Ware on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
